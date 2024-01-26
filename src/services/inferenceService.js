a57083469f5ed716b0228d733eb49a115441b16a
Ridha Ginanjar <ridhaginanjar7@gmail.com>
2024-01-26 10:55:15 +0700

    Update inferenceService.js via Web

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -21,19 +21,19 @@ async function predictClassification(model, image) {
     let explanation, suggestion;
 
     if (label === 'Melanocytic nevus') {
-      explanation = "Melanocytic nevus adalah kondisi di mana permukaan kulit memiliki bercak warna yang berasal dari sel-sel melanosit, yakni pembentukan warna kulit dan rambut."
-      suggestion = "Segera konsultasi dengan dokter terdekat jika ukuran semakin membesar dengan cepat, mudah luka atau berdarah."
+      explanation = "Melanocytic nevus adalah kondisi permukaan kulit memiliki bercak warna yang berasal dari sel-sel melanosit, yakni pembentukan warna kulit dan rambut."
+      suggestion = "Segera konsultasi dengan dokter terdekat jika ukuran semakin membesar dengan cepat, mudah luka, atau berdarah."
     }
-
+  
     if (label === 'Squamous cell carcinoma') {
       explanation = "Squamous cell carcinoma adalah jenis kanker kulit yang umum dijumpai. Penyakit ini sering tumbuh pada bagian-bagian tubuh yang sering terkena sinar UV."
       suggestion = "Segera konsultasi dengan dokter terdekat untuk meminimalisasi penyebaran kanker."
     }
-
+  
     if (label === 'Vascular lesion') {
-      explanation = "Vascular lesion adalah penyakit yang dikategorikan sebagai kanker atau tumor di mana penyakit ini sering muncul pada bagian kepala dan leher."
+      explanation = "Vascular lesion adalah penyakit yang dikategorikan sebagai kanker atau tumor. Penyakit ini sering muncul pada bagian kepala dan leher."
       suggestion = "Segera konsultasi dengan dokter terdekat untuk mengetahui detail terkait tingkat bahaya penyakit."
-
+  
     }
 
     return { confidenceScore, label, explanation, suggestion };
